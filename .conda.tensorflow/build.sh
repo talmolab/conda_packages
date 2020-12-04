fc7c31ad564938176d9e66c72e1d32a60eef4c52
Talmo Pereira <talmo@princeton.edu>
2020-12-04 16:24:03 -0500

    Expand package summary on Anaconda - Pin numpy to 1.18.1 - Test all imports

--- .conda.tensorflow/build.sh
+++ .conda.tensorflow/build.sh
@@ -4,7 +4,7 @@ export PIP_NO_INDEX=False
 export PIP_NO_DEPENDENCIES=False
 export PIP_IGNORE_INSTALLED=False
 
-pip install "numpy>=1.18.1,<1.19.0"
+pip install numpy==1.18.1
 pip install tensorflow==2.3.1
 pip install tensorboard==2.3
 pip install h5py==2.10.0
