10d1e6570dcb8c9c76974239fea353841bc95b81
Talmo Pereira <talmo@salk.edu>
2022-02-22 17:00:28 -0800

    Tensorflow 2.6 package with more permissive conda-based reqs

--- .conda.tensorflow/bld.bat
+++ .conda.tensorflow/bld.bat
@@ -4,10 +4,11 @@ set PIP_NO_INDEX=False
 set PIP_NO_DEPENDENCIES=False
 set PIP_IGNORE_INSTALLED=False
 
-rem pip install numpy==1.21.2
-pip install numpy==1.19.5
-pip install tensorflow==2.7.0
-pip install tensorboard==2.7.0
-pip install h5py==3.1.0
-pip install opencv-python-headless==4.2.0.34
+pip install numpy==1.21.5
+rem pip install numpy==1.19.5
+pip install tensorflow==2.6.3
+pip install tensorboard==2.6.3
+rem pip install h5py==3.1.0
+pip install opencv-python-headless==4.5.5.62
+pip install git+https://github.com/talmolab/wrap_opencv-python-headless.git@ede49f6a23a73033216339f29515e59d594ba921
 pip install setuptools-scm
