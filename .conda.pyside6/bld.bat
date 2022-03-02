d213034a9200e5385b128313f31252d3847ba8b9
Talmo Pereira <talmo@princeton.edu>
2022-03-01 22:23:12 -0800

    Add PySide6

---
+++ .conda.pyside6/bld.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set PIP_NO_INDEX=False
+set PIP_NO_DEPENDENCIES=False
+set PIP_IGNORE_INSTALLED=False
+
+pip install PySide2==6.2.2.1
+rem pip install numpy==1.19.5
+rem pip install wheel==0.32.0
+pip install setuptools-scm
---
+++ .conda.pyside6/bld.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export PIP_NO_INDEX=False
+export PIP_NO_DEPENDENCIES=False
+export PIP_IGNORE_INSTALLED=False
+
+pip install PySide2==6.2.2.1
+# pip install numpy==1.19.5
+# pip install wheel==0.32.0
+pip install setuptools-scm
