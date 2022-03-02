7e585d461f5881e5cdff4128b316c771e986e38f
Talmo Pereira <talmo@princeton.edu>
2022-03-01 22:37:31 -0800

    Fix package name in build

--- .conda.pyside6/bld.bat
+++ .conda.pyside6/bld.bat
@@ -4,7 +4,7 @@ set PIP_NO_INDEX=False
 set PIP_NO_DEPENDENCIES=False
 set PIP_IGNORE_INSTALLED=False
 
-pip install PySide2==6.2.2.1
+pip install PySide6==6.2.2.1
 rem pip install numpy==1.19.5
 rem pip install wheel==0.32.0
 pip install setuptools-scm
